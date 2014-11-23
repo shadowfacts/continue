3edbd71b07919a14612ccb102e6a14b36d80a9c6
Shadowfacts <akorde@icloud.com>
2014-11-22 21:44:53 -0500

    Woohoo! Basic building! And Cloning, and running shell scripts!

--- app.js
+++ app.js
@@ -11,11 +11,14 @@
 
 // Modules
 var path = require('path'),
+	fs = require('fs'),
+	exec = require('child_process').exec,
 	express = require('express'),
 	morgan = require('morgan'),
 	favicon = require('serve-favicon'),
 	bodyParser = require('body-parser'),
-	multer = require('multer');
+	multer = require('multer'),
+	git = require('nodegit');
 
 // The App!
 var app = express();
@@ -33,6 +36,22 @@ app.set('port', process.env.PORT || 8080);
 
 app.get('/', function(req, res) {
 	res.render('index', { title: 'Continue | Super-Awesome Continuous Integration' });
+});
+
+app.post('/build', function(req, res) {
+	console.log('Building ' + req.param('user') + '/' + req.param('repo'));
+	var localPath = path.join(__dirname, 'build', req.param('user'), req.param('repo'));
+	var githubPath = 'git://github.com/' + req.param('user') + '/' + req.param('repo') + '.git';
+	git.Clone.clone(githubPath, localPath).then(function(repo) {
+		var buildPath = path.join(__dirname, 'build', req.param('user'), req.param('repo'));
+		var buildScript = 'cd ' + buildPath + '; git submodule init; git submodule update; chmod +x ./gradlew; ./gradlew clean setupCIWorkSpace build;';
+		res.end();
+		var child = exec(buildScript, function(err, stdout, stderr) {
+			if (err == null) {
+				console.log('Yay! Build finished!');
+			}
+		});
+	});
 });
 
 
